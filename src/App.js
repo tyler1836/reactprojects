ead067dae2c9a420558ec6cd827238775474bc21
unknown <tyler.r.roll@gmail.com>
2021-11-29 16:40:05 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menu, setMenu] = useState(items);
+  const [categories, setCategories] = useState([]);
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu items={menu}/>
+      </section>
+    </main>
+  );
+}
+
+export default App;
