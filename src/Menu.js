e4eb43a9f1a2e2e419582c118e655fda24320cf6
unknown <tyler.r.roll@gmail.com>
2021-11-29 17:17:38 -0700

    react dynamic button render

--- src/Menu.js
+++ src/Menu.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const Menu = ({items}) => {
+const Menu = ({ items }) => {
 
   return (
-  <div className='section-center'>
-    {items.map((menuItem) => {
-      const {id, title, img, desc} = menuItem;
-      return <article key={id} className='menu-item'>
-        <img src={img} alt={title} className='photo'/>
-
-      </article>
-    })}
-  </div>
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const { id, title, img, price, desc } = menuItem;
+        return <article key={id} className='menu-item'>
+          <img src={img} alt={title} className='photo' />
+          <div className='item-info'>
+            <header>
+              <h4>
+                {title}
+              </h4>
+              <h4 className='price'>{price}</h4>
+            </header>
+            <p className='item-text'>
+              {desc}
+            </p>
+          </div>
+        </article>
+      })}
+    </div>
   );
 };
 
